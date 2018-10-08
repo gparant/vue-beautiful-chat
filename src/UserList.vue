93f4c76e92eabfe819cf7bfb9fb6207f6f031f4f
grégoire parant <g.parant@thecodingmachine.com>
2018-10-08 13:12:28 +0200

    Update User List
     - Add avatar html of user

--- src/UserList.vue
+++ src/UserList.vue
@@ -3,7 +3,10 @@
     <table class="" style="padding-top: 5px">
       <tbody>
         <tr v-for="user in participants" :key="user.id">
-            <td style="text-align: center;"><img :src="user.imageUrl" class="img-msg"/></td>
+            <td v-if="user.avatarHtml" class="avatar">
+              <span class="avatar-html" v-html="user.avatarHtml"></span>
+            </td>
+            <td v-else style="text-align: center;"><img :src="user.imageUrl" class="img-msg"/></td>
             <td>{{user.name}}</td>
         </tr>
       </tbody>
@@ -31,5 +34,16 @@ export default {
     border-radius: 50%;
     width: 50px;
     margin-right: 5px;
+  }
+  table {
+    width: 100%;
+  }
+  td.avatar{
+    padding-left: 20px;
+    width: 20%;
+  }
+  td{
+    text-align: left;
+    vertical-align: middle;
   }
 </style>
