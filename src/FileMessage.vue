bdd21f7458e91b122cb4481ee6b7828a7ef45c41
grégoire parant <g.parant@thecodingmachine.com>
2018-10-08 12:54:22 +0200

    Update message List
     - Add avatar HTML

--- src/FileMessage.vue
+++ src/FileMessage.vue
@@ -1,107 +1,82 @@
 <template>
-  <div class='sc-message--file' :style="messageColors">
-    <div class='sc-message--file-icon'>
-      <a :href="data.file.url || '#'" target='_blank'>
-        <img src="./assets/file.svg" alt='generic file icon' height="60" />
-      </a>
-    </div>
-    <div class='sc-message--file-name' :style="messageColors">
-      <a :href="data.file.url ? data.file.url : '#'" target='_blank'>{{data.file.name || ''}}</a>
-    </div>
-    <div class="sc-message--file-text" :style="messageColors">{{data.text}}<p v-if="data.meta" class='sc-message--meta' :style="messageColors">{{data.meta}}</p></div>
+  <div class="sc-message-list" ref="scrollList" :style="{backgroundColor: colors.messageList.bg}">
+    <Message v-for="(message, idx) in messages" :message="message" :chatImageUrl="chatImageUrl(message.author)" :authorName="authorName(message.author)" :avatarHtml="chatImageUrl(message.author)" :key="idx" :colors="colors" />
+    <Message v-show="showTypingIndicator !== ''" :message="{author: showTypingIndicator, type: 'typing'}" :chatImageUrl="chatImageUrl(showTypingIndicator)" :colors="colors" />
   </div>
 </template>
-
 <script>
+import Message from './Message.vue'
+import chatIcon from './assets/chat-icon.svg'
+
 export default {
+  components: {
+    Message
+  },
   props: {
-    data: {
+    participants: {
+      type: Array,
+      required: true
+    },
+    messages: {
+      type: Array,
+      required: true
+    },
+    showTypingIndicator: {
+      type: String,
+      required: true
+    },
+    colors: {
       type: Object,
       required: true
     },
-    messageColors: {
-      type: Object,
+    alwaysScrollToBottom: {
+      type: Boolean,
       required: true
     }
+  },
+  methods: {
+    _scrollDown () {
+      this.$refs.scrollList.scrollTop = this.$refs.scrollList.scrollHeight
+    },
+    shouldScrollToBottom() {
+      return this.alwaysScrollToBottom || (this.$refs.scrollList.scrollTop > this.$refs.scrollList.scrollHeight - 600)
+    },
+    profile(author) {
+      const profile = this.participants.find(profile => profile.id === author)
+
+      // A profile may not be found for system messages or messages by 'me'
+      return profile || {imageUrl: '', name: '', avatarHtml: ''};
+    },
+    chatImageUrl(author) {
+      return this.profile(author).imageUrl;
+    },
+    authorName(author) {
+      return this.profile(author).name;
+    },
+    chatImageUrl(author) {
+      return this.profile(author).avatarHtml;
+    },
+  },
+  computed: {
+    defaultChatIcon() {
+      return chatIcon
+    }
+  },
+  mounted () {
+    this._scrollDown()
+  },
+  updated () {
+    if (this.shouldScrollToBottom())
+      this.$nextTick(this._scrollDown())
   }
 }
 </script>
 
 <style scoped>
-.sc-message--file {
-  border-radius: 6px;
-  font-weight: 300;
-  font-size: 14px;
-  line-height: 1.4;
-  /* white-space: pre-wrap; */
-  -webkit-font-smoothing: subpixel-antialiased
-}
-
-.sc-message--content.sent .sc-message--file {
-  word-wrap: break-word;
-}
-
-.sc-message--file-icon {
-  text-align: center;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 15px;
-  margin-bottom: 0px;
-}
-
-.sc-message--file-icon:hover {
-  opacity: 0.7;
-}
-
-.sc-message--file-text {
-  padding: 17px 20px;
-  border-radius: 6px;
-  font-weight: 300;
-  font-size: 14px;
-  line-height: 1.4;
-  white-space: pre-wrap;
-  -webkit-font-smoothing: subpixel-antialiased
-}
-
-.sc-message--file-name {
-  color: white;
-  padding-left: 15px;
-  padding-right: 15px;
-  padding-top: 0;
-  font-size: x-small;
-  text-align: center;
-}
-
-.sc-message--file-name a {
-  text-decoration: none;
-  color: #ece7e7;
-}
-
-.sc-message--file-name a:hover {
-  color: white;
-}
-
-.sc-message--content.sent .sc-message--file-text {
-  color: white;
-  background-color: #4e8cff;
-  word-wrap: break-word;
-}
-
-.sc-message--content.received .sc-message--file {
-  color: #263238;
-  background-color: #f4f7f9;
-  margin-right: 40px;
-}
-
-.sc-message--content.received .sc-message--file-name {
-  color: #000;
-}
-
-.sc-message--content.received .sc-message--file a {
-  color: rgba(43, 40, 40, 0.7);
-}
-
-.sc-message--content.received .sc-message--file a:hover {
-  color: #0c0c0c;
+.sc-message-list {
+  height: 80%;
+  overflow-y: auto;
+  background-size: 100%;
+  padding: 40px 0px;
 }
 </style>
